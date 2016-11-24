f3b7bf36f6e1476038fd861dd6185bddeaf1a8ae
dingxue <304875853@qq.com>
2016-11-24 10:20:23 +0800

    eslint code

--- src/provider.js
+++ src/provider.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
-import { Router, Route, browserHistory } from 'react-router';
-import { Provider, observer } from 'mobx-react'
+import React, { PureComponent } from 'react';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'mobx-react';
 import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+/*eslint-disable */
 import DevTools from 'mobx-react-devtools';
 import AppState from './components/app/AppState';
+//  当前语句解决require.ensure 的热加载问题，也可以在源头处解决
+/*eslint-disable */
 import Index from './components/app/App.jsx';
 import createRoute from './route';
 // const appState = new AppState();
@@ -14,18 +17,17 @@ const stores = {
   routing: routingStore,
   // ...other stores
 };
-//<Router history={history} routes={createRoute} />
-const history = syncHistoryWithStore(browserHistory, routingStore)
+//  <Router history={history} routes={createRoute} />
+const history = syncHistoryWithStore(browserHistory, routingStore);
 
-class App extends Component {
+class App extends PureComponent {
   render() {
-    
     return (
       <Provider store={stores}>
         <Router history={history} routes={createRoute} />
       </Provider>
     );
   }
-};
+}
 
 export default App;
