a8d7e74eb285e6047019e97e27613fd4c8ae7ddd
dingxue <304875853@qq.com>
2016-11-24 17:29:38 +0800

    test

--- src/index.prod.jsx
+++ src/index.prod.jsx
@@ -2,23 +2,22 @@ require("react-hot-loader/patch")
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './provider';
+// import App from './provider';
+import Testing from './test';
 
 render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
+  <Testing />,
   document.getElementById('root')
 );
 
-if (module.hot) {
-  module.hot.accept('./provider', () => {
-    var NewApp = require('./provider').default;
-    render(
-      <AppContainer>
-        <NewApp />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
-}
+// if (module.hot) {
+//   module.hot.accept('./provider', () => {
+//     var NewApp = require('./provider').default;
+//     render(
+//       <AppContainer>
+//         <NewApp />
+//       </AppContainer>,
+//       document.getElementById('root')
+//     );
+//   });
+// }
