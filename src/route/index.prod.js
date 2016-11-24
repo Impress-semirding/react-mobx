fc566257657a8dca609e95b3d6da51e75d22c9c5
dingxue <304875853@qq.com>
2016-11-24 21:36:59 +0800

    asycn store

--- src/route/index.prod.js
+++ src/route/index.prod.js
@@ -1,7 +1,12 @@
+import { injectAsyncReducer } from '../store';
+import AppState from './../components/app/AppState';
+
 const Index = {
   path: 'index',
   getComponent: (nextState, cb) => {
     require.ensure([], (require) => {
+      const appState = new AppState();
+      injectAsyncReducer('asycn', appState);
       cb(null, require('../components/app/App.jsx').default);
     }, 'index');
   },
