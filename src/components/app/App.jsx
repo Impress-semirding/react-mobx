ddd4d4b9f36ee5f7306e4ad8fdf6dfc467e9e898
dingxue <304875853@qq.com>
2016-11-24 20:49:40 +0800

    preload eslint

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
-@inject("store") @observer
+@inject('store') @observer
 class App extends Component {
+  onReset = () => {
+    this.props.store.AppState.resetTimer();
+  }
   render() {
     const { AppState } = this.props.store;
     return (
       <div>
         <button onClick={this.onReset}>
-          Seconds  -hah dingxue: {AppState.timer}
+          Seconds  -ha: {AppState.timer}
         </button>
         <DevTools />
       </div>
     );
   }
+}
 
-  onReset = () => {
-    this.props.store.AppState.resetTimer();
-  }
+App.propTypes = {
+  store: React.PropTypes.object.isRequired
 };
 
 export default App;
